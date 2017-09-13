25ef6471f2c4825f32d6b5d686150843ecb34177
Anthony Ramoslebron <ajramoslebron@gmail.com>
2017-09-13 14:10:18 -0400

    Added Line path of user while user is walking.

--- app/src/main/java/edu/fullsail/mgems/cse/gps_showcase/MainActivity.java
+++ app/src/main/java/edu/fullsail/mgems/cse/gps_showcase/MainActivity.java
@@ -3,6 +3,7 @@ package edu.fullsail.mgems.cse.gps_showcase;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.media.audiofx.BassBoost;
 import android.os.Build;
 import android.provider.Settings;
@@ -28,6 +29,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 
 //Really Good Help
@@ -44,6 +50,8 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private TextView lat;
     private String provider;
 
+    private Polyline line;
+    private ArrayList<LatLng> points;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         button.setOnTouchListener(this);
         lon = (TextView) findViewById(R.id.lon);
         lat = (TextView) findViewById(R.id.lat);
+        points = new ArrayList<LatLng>();
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationListener = new LocationListener() {
@@ -65,10 +74,11 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             public void onLocationChanged(Location location) {
                 lon.setText("Lon:" + " " + location.getLongitude());
                 lat.setText("Lat:" + " " + location.getLatitude());
-
+                LatLng latLon = new LatLng(location.getLatitude(),location.getLongitude());
                 //mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude())));
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()), 16.0f));
-
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLon, 17.0f));
+                points.add(latLon);
+                ReDrawLine();
             }
 
             @Override
@@ -104,23 +114,33 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     }
 
+    private void ReDrawLine()
+    {
+        mMap.clear();
+
+        PolylineOptions options = new PolylineOptions();
+        options.width(5);
+        options.color(Color.BLUE);
+        options.geodesic(true);
+
+        for (int i = 0; i < points.size(); i++)
+        {
+            options.add(points.get(i));
+        }
+
+        line = mMap.addPolyline(options);
+    }
     private void ConfigureButton()
     {
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             Toast.makeText(this,"GPS Signal not active", Toast.LENGTH_LONG).show();
             return;
         }
         locationManager.requestLocationUpdates("gps", 100, 1, locationListener);
+        //locationManager.requestLocationUpdates();
     }
 
     @Override
@@ -138,17 +158,12 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             Toast.makeText(this,"GPS Signal not active", Toast.LENGTH_LONG).show();
             return false;
         }
         locationManager.requestLocationUpdates("gps", 100, 1, locationListener);
+        //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()), 16.0f));
+
         return false;
     }
 
@@ -160,5 +175,19 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         LatLng FullSail = new LatLng(28.5962177, -81.3414085);
         mMap.addMarker(new MarkerOptions().position(FullSail).title("Marker at Full Sail University"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(FullSail));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(this,"GPS Signal not active", Toast.LENGTH_LONG).show();
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
 }
